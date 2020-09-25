9397fd1c1bf4cd4731f4d3705d06e014c474505c
Julien Klaer <julien.klaer@nextgatetech.com>
2020-09-25 12:27:53 +0200

    Fixing test

--- internal/pkg/utils/project_test.go
+++ internal/pkg/utils/project_test.go
@@ -2,14 +2,13 @@ package utils
 
 import (
 	"os"
-	"path/filepath"
 	"testing"
 )
 
 func TestBasePath(t *testing.T) {
 	bp := BasePath()
-	gopath := os.Getenv("GOPATH")
-	pp := filepath.Join(gopath, "src", "github.com", "MadJlzz", "gopypi")
+	_ = os.Chdir("../../..")
+	pp, _ := os.Getwd()
 	if bp != pp {
 		t.Errorf("base path is not set to the root of the project.\ngot: [%s], want: [%s]", pp, bp)
 	}
